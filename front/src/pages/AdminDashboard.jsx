30f994ebd7358b07258009d4cd511a2828032cd3
longonicolas <nlongo@frba.utn.edu.ar>
2025-07-24 12:55:33 -0300

    Revert "funcionalidad busqueda de preguntas"
    This reverts commit e698775e293ebe4cdbe7d87e9562e0808f6bbfd1.

--- front/src/pages/AdminDashboard.jsx
+++ front/src/pages/AdminDashboard.jsx
@@ -1,4 +1,3 @@
-// src/pages/AdminDashboard.jsx
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@radix-ui/react-tabs";
 import PreguntaForm from "../components/PreguntaForm";
 import EncuestaForm from "../components/EncuestaForm";
